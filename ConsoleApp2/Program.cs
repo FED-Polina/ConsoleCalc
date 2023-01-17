9d31ec6d6d87f2cee13ba55ac2c8377f30e2ffd3
Polina Fedulova <FEDPolina_A@bk.ru>
2023-01-17 12:37:02 +0300

    Addition

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            double a = 2; double b = 3; double c = 0;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+            
+        }
+    }
+}

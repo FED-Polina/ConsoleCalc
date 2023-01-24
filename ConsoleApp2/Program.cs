8382f169892691944eea65b3f6a839982c81328e
Polina Fedulova <FEDPolina_A@bk.ru>
2023-01-24 12:11:00 +0300

    basecalc

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,11 +11,19 @@ namespace ConsoleApp2
         static void Main(string[] args)
 
         {
-            double a = 7; double b = 6; double c = 0;
+            double a = 7; double b = 6; double c = 0; double d = 0; double f = 0;double g = 0;
             c = a + b;
+            d = a - b;
+            f = a * b; 
+            g = a / b;
             //after reset
             Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
             Console.ReadLine();
+
+
             
         }
     }
